65a072ca09d6658eef26dbc4eb9c7b5dea557113
Tanya Aleksandrova <tan-aleksandrova2102@yandex.ru>
2022-07-13 11:21:17 +0300

    Обновила checkNameToEmboss() на проверку с помощью регулярных выражений

--- src/main/java/Account.java
+++ src/main/java/Account.java
@@ -7,17 +7,11 @@ public class Account {
     }
 
     public boolean checkNameToEmboss ()  {
-        int i=0;
 //      в строке не меньше 3 и не больше 19 символов
-        if (name != null &&  name.length() >= 3 && name.length() <= 19) i++;
-            else return  false;
 //      в строке есть только один пробел
 //      пробел стоит не в начале и не в конце строки.
-        if (StringUtils.countMatches(name," ") != 1
-                || name.startsWith(" ")  || name.endsWith(" ") )   return  false;
-            else i++;
+        return (name !=null && name.matches("^[^\\s]\\S*\\s{1}\\S*[^\\s]$") && name.matches(".{3,19}")) ;
 
-     return  i == 2 ?  true : false;
     }
 
 }
